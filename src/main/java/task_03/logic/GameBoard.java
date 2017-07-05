e4197ffdcbf89f4c1d0e29a6632d0fe7e25377f7
Venreaver <reaversindustry@gmail.com>
2017-07-05 19:44:51 +0300

    Class GameBoard was updated

--- src/main/java/task_03/logic/GameBoard.java
+++ src/main/java/task_03/logic/GameBoard.java
@@ -13,13 +13,10 @@ public class GameBoard {
     private List<Ship> shipList;
 
     GameBoard() {
-        long startTime = System.nanoTime();
         this.cells = new Cell[GameConfig.BOARD_SIZE][GameConfig.BOARD_SIZE];
         this.availableCellList = new ArrayList<>(GameConfig.BOARD_SIZE * GameConfig.BOARD_SIZE);
         this.shipList = new ArrayList<>();
         makeRandomBoard();
-        long time = System.nanoTime() - startTime;
-        System.out.println(time);
     }
 
     private void makeRandomBoard() {
