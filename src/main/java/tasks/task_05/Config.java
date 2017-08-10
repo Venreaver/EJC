7691e4ecffa5d5409febf8334ba05a1022343577
Irina Vasileva <reaversindustry@gmail.com>
2017-08-10 08:26:43 +0300

    Refactored code

--- src/main/java/tasks/task_05/Config.java
+++ src/main/java/tasks/task_05/Config.java
@@ -4,6 +4,7 @@ class Config {
     static final String INPUT_FOLDER_PATH = "./src/main/java/tasks/task_05/data";
     static final String OUTPUT_FOLDER_NAME = "/output";
     static final String OUTPUT_FILE_NAME = "/Report";
+    static final String FILE_EXTENTION = ".csv";
     static final String REPORT_ATTRIBUTES = "user,url,time";
     static final String ERROR = "Error: ";
     static final int THREAD_COUNT = 10;
